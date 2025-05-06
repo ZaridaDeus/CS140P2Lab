c62de9270d24c7f5ae07f639a3e573f950875561
ZaridaDeus <144716093+ZaridaDeus@users.noreply.github.com>
2025-05-06 11:04:08 -0700

    Update ex1.asm

--- ex1.asm
+++ ex1.asm
@@ -1,32 +1,39 @@
 .data
-    ival:   .word 10
-    jval:   .word 20
-    g:      .word 100
-    h:      .word 200
-    fval:   .word 0
+    ival:   .word 100       # i = 100;
+    jval:   .word 200       # j = 200;
+    fval:   .word 0         # f = 0;
+    gval:   .word 10        # g = 10;
+    hval:   .word 5         # h = 5;
 
 .text
 .globl main
 
 main:
-    lw  $t1, g
-    lw  $t2, h
-    lw  $t3, ival
-    lw  $t4, jval
+    lw  $t0, fval           # $t0 = f;
+    lw  $t1, gval           # $t1 = g;
+    lw  $t2, hval           # $t2 = h;
 
-    bne $t3, $t4, false
-    add $t0, $t1, $t2
-    j endif
+    lw  $t3, ival           # $t3 = i;
+    lw  $t4, jval           # $t4 = j;
 
-false:
-    sub $t0, $t1, $t2
+    bne $t3, $t4, false     # if (i != j) jump to false
+    add $t0, $t1, $t2       # $t0 = $t1 + $t2
+    j endif                 
+
+false:                      # else
+    # xor $t2, $t2, 0xFFFF  1's complement -> eg: 001010 > 110101
+    lui $t5, 0xFFFF         # $t5 = 0xFFFFXXXX;
+    ori $t5, $t5, 0xFFFF    # $t5 = 0xFFFFFFFF;
+    xor $t2, $t2, $t5       # $t2 compares to $t5, to 1's complement
+    addi $t2, $t2, 1        # $t2 = $t2 + 1 for 2's complement -> $t2 = -$t2;
+    add $t0, $t1, $t2       # $t0 = $t1 + $t2;
 
 endif:
-    sw $t0, fval
+    sw  $t0, fval           # f = $t0;
 
-    li $v0, 1
-    move $a0, $t0
+    li  $v0, 1              
+    move $a0, $t0           # cout << f;
     syscall
 
-    li $v0, 10
+    li  $v0, 10
     syscall
